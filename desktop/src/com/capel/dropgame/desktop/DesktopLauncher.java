271cca390f69344d2d6d6accaa005215b878a53e
JCapelDev <win200003@gmail.com>
2021-01-14 18:12:02 +0100

    Fitxers basics afegits

---
+++ desktop/src/com/capel/dropgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.capel.dropgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.capel.dropgame.DropGameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DropGameMain(), config);
+	}
+}
